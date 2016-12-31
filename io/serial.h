68bbeea19b27b535b7f2dca77ba151b6b5e56621
Ryan Huard <ryan.huard@live.ca>
2016-12-30 23:35:35 -0700

    Serial stuff

---
+++ io/serial.h
@@ -0,0 +1,25 @@
+#ifndef SERIAL_H
+#define SERIAL_H
+
+/** serial_configure_baud_rate:
+*  Sets the speed of the data being sent. The default speed of a serial
+*  port is 115200 bits/s. The argument is a divisor of that number, hence
+*  the resulting speed becomes (115200 / divisor) bits/s.
+*
+*  @param com      The COM port to configure
+*  @param divisor  The divisor
+*/
+void serial_configure_baud_rate(unsigned short com, unsigned short divisor);
+
+/** serial_configure_line:
+*  Configures the line of the given serial port. The port is set to have a
+*  data length of 8 bits, no parity bits, one stop bit and break control
+*  disabled.
+*
+*  @param com  The serial port to configure
+*/
+void serial_configure_line(unsigned short com);
+
+int serial_write(char * buf, unsigned int len);
+
+#endif /* SERIAL_H */
--- loader.s
+++ loader.s
@@ -22,7 +22,8 @@ global loader                   ; the entry symbol for ELF
         mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
         mov esp, kernel_stack + KERNEL_STACK_SIZE   ; point esp to the start of the
                                                 ; stack (end of memory area)
-
+    extern main
+        ;call main
     extern write
         push dword 11
         push dword text
