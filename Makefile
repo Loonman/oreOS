eb303cb0441f7972f020c15b141c4fd27fbd0370
Ryan Huard <rhuard@ualberta.ca>
2016-12-30 17:52:45 -0700

    Whoops

--- Makefile
+++ Makefile
@@ -1,19 +1,36 @@
-.PHONY: iso
+OBJECTS = loader.o kmain.o
+CC = gcc
+CFLAGS = -m32 -nostdlib -nostdinc -fno-builtin -fno-stack-protector -nostartfiles -nodefaultlibs -Wall -Wextra -Werror -c
+LDFLAGS = -T link.ld -melf_i386
+AS = nasm
+ASFLAGS = -f elf
+
+all: kernel.elf
+
+kernel: $(OBJECTS)
+	ld $(LDFLAGS) $(OBJECTS) -o kernel.elf
 
 iso: kernel
-	genisoimage -R \
-            -b boot/grub/stage2_eltorito \
-            -no-emul-boot \
-            -boot-load-size 4 \
-            -A oreOS \
-            -input-charset utf8 \
-            -quiet \
-            -boot-info-table \
-            -o oreOS.iso \
+	cp kernel.elf iso/boot/kernel.elf
+	genisoimage -R                              \
+            -b boot/grub/stage2_eltorito    \
+            -no-emul-boot                   \
+            -boot-load-size 4               \
+            -A os                           \
+            -input-charset utf8             \
+            -quiet                          \
+            -boot-info-table                \
+            -o oreOS.iso                       \
             iso
 
-kernel: link.ld loader.o
-	ld -T link.ld -melf_i386 loader.o -o kernel.elf
+run: iso
+	bochs -f bochsrc.txt -q
 
-loader.o : loader.s
-	nasm -f elf32 loader.s
+%.o: %.c
+	$(CC) $(CFLAGS)  $< -o $@
+
+%.o: %.s
+	$(AS) $(ASFLAGS) $< -o $@
+
+clean:
+	rm -rf *.o kernel.elf os.iso
--- loader.s
+++ loader.s
@@ -1,9 +1,17 @@
 global loader                   ; the entry symbol for ELF
-
+    
     MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
     FLAGS        equ 0x0            ; multiboot flags
     CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
                                     ; (magic number + checksum + flags should equal 0)
+    KERNEL_STACK_SIZE equ 4096      ; size of stack in bytes
+
+    section .bss
+    align 4                                     ; align at 4 bytes
+    kernel_stack:                               ; label points to beginning of memory
+        resb KERNEL_STACK_SIZE                  ; reserve stack for the kernel
+        mov esp, kernel_stack + KERNEL_STACK_SIZE   ; point esp to the start of the
+                                                ; stack (end of memory area)
 
     section .text:                  ; start of the text (code) section
     align 4                         ; the code must be 4 byte aligned
@@ -13,5 +21,14 @@ global loader                   ; the entry symbol for ELF
 
     loader:                         ; the loader label (defined as entry point in linker script)
         mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
+        
+    extern sum_of_three
+        ; The assembly code
+        push dword 3            ; arg3
+        push dword 2            ; arg2
+        push dword 1            ; arg1
+        call sum_of_three       ; call the function, the result will be in eax
+    
     .loop:
+        
         jmp .loop                   ; loop forever
