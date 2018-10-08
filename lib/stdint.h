39a9325e69db385b7d5ebfe4739f361a82ce862c
Ryan Huard <ryan.huard@live.ca>
2018-10-08 12:13:37 -0700

    Add signed fixed width integers

--- lib/stdint.h
+++ lib/stdint.h
@@ -1,9 +1,20 @@
 #ifndef STDINT_H
 #define STDINT_H
 
+/**
+ * Unsigned fixed width integers
+ */
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 typedef unsigned long long uint64_t;
+
+/**
+ * Signed fixed width integers
+ */
+typedef char uint8_t;
+typedef short uint16_t;
+typedef int uint32_t;
+typedef long long uint64_t;
 
 #endif /* STDINT_H */
