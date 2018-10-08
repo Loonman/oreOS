c1c1d817671b2e2ced248a6cb097310fa3351b43
Ryan Huard <ryan.huard@live.ca>
2018-10-08 12:24:39 -0700

    Update IO header to use fixed width integers

--- io/io.h
+++ io/io.h
@@ -1,6 +1,7 @@
 #ifndef INCLUDE_IO_H
 #define INCLUDE_IO_H
 
+#include "../lib/stdint.h"
 /* The I/O ports */
 #define FB_COMMAND_PORT         0x3D4
 #define FB_DATA_PORT            0x3D5
@@ -15,7 +16,7 @@
 *  @param port The I/O port to send the data to
 *  @param data The data to send to the I/O port
 */
-void outb(unsigned short port, unsigned char data);
+void outb(uint16_t port, uint8_t data);
 
 
 /** inb:
@@ -24,6 +25,6 @@ void outb(unsigned short port, unsigned char data);
 *  @param  port The address of the I/O port
 *  @return      The read byte
 */
-unsigned char inb(unsigned short port);
+uint8_t inb(uint16_t port);
 
 #endif /* INCLUDE_IO_H */
