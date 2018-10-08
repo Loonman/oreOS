aaed859bb47f499776be1ea6ffe5272f1e4aec3f
Ryan Huard <ryan.huard@live.ca>
2018-10-08 12:39:04 -0700

    Logging library uses fixed width integers

--- lib/stdint.h
+++ lib/stdint.h
@@ -7,7 +7,9 @@
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
+#ifdef __64_BIT
 typedef unsigned long long uint64_t;
+#endif // __64_BIT
 
 /**
  * Signed fixed width integers
@@ -15,6 +17,14 @@ typedef unsigned long long uint64_t;
 typedef char uint8_t;
 typedef short uint16_t;
 typedef int uint32_t;
+#ifdef __64_BIT
 typedef long long uint64_t;
+#endif // __64_BIT
+
+#ifndef __64_BIT
+typedef size_t uint32_t;
+#else // __64_BIT
+typedef size_t uint64_t;
+#endif // __64_BIT
 
 #endif /* STDINT_H */
